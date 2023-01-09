594325b7a685fab714cb24fcd5d1348acc53e4bd
he22ra <hhraleeee@gmail.com>
2023-01-09 17:53:06 +0900

    tensor torch, google colab 실습

--- Python,DL/DL4/torch_basic4.py
+++ Python,DL/DL4/torch_basic4.py
@@ -1,52 +1,88 @@
+'''
+
+'''
 import torch
-from torch import nn
-from torch.utils.data import DataLoader
-from torchvision import datasets
-from torchvision.transforms import ToTensor, Lambda, Compose
-import matplotlib.pyplot as plt
 import numpy as np
 
-# minist data down
-training_data = datasets.MNIST(
-    root='data',
-    train=True,
-    download=True,
-    transform=ToTensor(),
-)
+#list로부터 tensor 생성
+data = [
+    [1, 2],
+    [3, 4]
+]
+x_data = torch.tensor(data)
+print(x_data)
 
-# test data
-test_data = datasets.MNIST(
-    root='data',
-    train=False,
-    download=True,
-    transform=ToTensor(),
-)
+# copy본 생성
+np_array = np.array(data)
+x_np_1 = torch.tensor(np_array)
+print(x_np_1)
 
-'''
-    * 데이터 준비
-        1) torch.utils.data import DataLoader
-           데이터로더 (DataLoader) 객체 
-            - 학습에 사용될 데이터 전체를 보관했다가 
-            - 모델 학습을 할 때 배치 크기만큼 데이터를 꺼내서 사용.
-        2) 내부적으로 반복자(iterator)에 포함된 인덱스를 이용해  배치 크기만큼 반환함 
-'''
-batch_size = 64
+#view를 만듦 (실체를 가리킴)
+x_np_2 = torch.as_tensor(np_array)
+print(x_np_2)
 
-# 데이터로더 생성
-train_dataloader = DataLoader(training_data, batch_size=batch_size)
-test_dataloader = DataLoader(test_data, batch_size=batch_size)
+x_np_3 = torch.as_tensor(np_array)
+print(x_np_3)
 
-for X, y in test_dataloader:
-    print("Shape of X : ", X.shape)
-    print("Shape of y : ", y.shape, y.dtype)
-    break
+x_np_1[0,0] = 5
+print(x_np_1)
+print(np_array)
 
-# 학습에 사용할 CPU나 GPU 장치를 얻기
+print('-------------------------------------')
+a = torch.ones(2, 3)
+print(a)
 
-# 모델을 정의 (클래스)
+b = torch.zeros(2, 3)
+print(b)
 
-# Loss 함수와 Optimizer 설정
+c = torch.full((2, 3), 2)
+print(c)
 
-# Training을 위한 함수
+# empty : 메모리 초기화가 안됐으면 다른 값이 나올 수 있음.
+d = torch.empty(2, 3)
+print(d)
 
-# Test를 위한 함수
+f = torch.rand(2, 3)
+print(f)
+
+# 음수(-)값도 포함
+g = torch.randn(2, 3)
+print(g)
+
+print(f"Shape of tensor : {f.shape}")
+print(f"Datatype of tensor : {f.dtype}")
+print(f"Device of tensor : {f.device}")
+
+print('-------------------------------------')
+a = torch.arange(1, 13).reshape(3, 4)
+print(a)
+
+# indexing
+print(a[1])     #2번째 줄(행)
+print(a[0, -1])     #1번째 줄(행)에서 끝에서 1번째
+
+# slicing
+print(a[1:-1])
+print(a[:2, 2:])        #
+
+x = torch.tensor([
+    [1, 2],
+    [3, 4]
+], dtype=torch.float32)
+
+y = torch.tensor([
+    [5, 6],
+    [7, 8]
+], dtype=torch.float32)
+
+print(x)
+print(y)
+
+print(x @ y)
+print('='*30)
+
+print(torch.add(x, y))
+print(torch.subtract(x, y))
+print(torch.multiply(x, y))
+print(torch.divide(x, y))
+print(torch.matmul(x, y))
